c2dce3b261da7e3c678c29c5ba7b8b84d9dba926
bala-amypo <bala@amypo.in>
2025-12-17 05:13:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,10 +1,53 @@
-import org.springframework.web.bind.annotation.RestController;
+package com.example.springFirst.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import sbs.rosedev.springFirst.entity.StudentEntity;
+import sbs.rosedev.springFirst.services.StudentService;
+
+
+
+
 
 @RestController
 public class StudentController {
-    @GetMapping("/get")
-    public StudentEntity getData() {
-        return 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
+
+    @PostMapping("/postStudentData")
+    public StudentEntity postStudentData(@RequestBody StudentEntity entity) {
+        return studentService.addRecord(entity);
+    }
+
+    @GetMapping("/getStudentData")
+    public List<StudentEntity> getStudentData() {
+        return studentService.getRecords();
+    }
+    
+    @GetMapping("/getStudentId/{id}")
+    public StudentEntity getStudentId(@PathVariable int id) {
+        return studentService.getStudentRecord(id);
+    }
+
+    @PutMapping("/updateStudentRecord/{id}")
+    public StudentEntity updateStudentData(@PathVariable int id, @RequestBody StudentEntity entity) {
+        return studentService.updateStudentRecord(id, entity);
+    }
+
+    @DeleteMapping("/deleteStudentRecord/{id}")
+    public String deleteStudentData(@PathVariable int id) {
+        return studentService.deleteStudentRecord(id);
+    }
+
 }
--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -1,8 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-impo
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepo  {
+import com.example.demo.entity.StudentEntity;
+
+@Repository
+public interface StudentRepo extends JpaRepository<StudentEntity, Long> {
 
 }
